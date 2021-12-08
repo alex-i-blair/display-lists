36fbd77a6a99eb4125f1095e24b9aebe70d7169a
Alex Blair <bclan11@gmail.com>
2021-12-07 18:19:53 -0800

    recipes display

---
+++ cocktails-data.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        name: 'Sazerac',
+        image: 'https://www.liquor.com/thmb/7CPeEMUEQLeAJgmVdyTRHpO4ix8=/735x0/pumpkin-sazerac-720x720-primary-62b0214f91904fdf962e9db2f06528ee.jpg',
+        recipe: {
+            'Rye Whiskey': '2oz',
+            'Rich Simple': '0.25oz',
+            "Peychaud's Bitters": '5 dashes',
+            'Angostura Bitters': '2 dashes'
+
+        },
+        garnish: 'Two expressed lemon peels'
+    },
+    {
+        name: 'Ramos Gin Fizz',
+        image: 'https://i0.wp.com/assets.punchdrink.com/wp-content/uploads/2018/06/Article-Ramos-Gin-Fizz-Plymouth-Gin-Cocktail-Recipe.jpg',
+        recipe: {
+            'Gin': '2oz',
+            'Rich Simple': '.75oz',
+            'Lemon': '.5oz',
+            'Lime': '.5oz',
+            'Club Soda': '1oz',
+        },
+        garnish: 'Express lemon peel'
+    },
+    {
+        name:'Whiskey Sour',
+        image: 'https://kitchenswagger.com/wp-content/uploads/2019/10/whiskey-sour22.jpg',
+        recipe: {
+            'Rye Whiskey (100 proof)': '2oz',
+            'Lemon': '1oz',
+            'Orange': '.5oz',
+            'Rich Simple': '.5oz',
+            'Egg White': '.5oz'
+        },
+        garnish: 'Lemon Twist'
+    },
+    {
+        name: 'Americano',
+        image: 'https://assets-prd.punchdrink.com/wp-content/uploads/2014/07/20105809/Social-Rome-with-A-View-Americano.jpg',
+        recipe: {
+            'Campari': '1.25oz',
+            'Sweet Vermouth': '1.25oz',
+            'Club Soda': '3oz',
+        },
+        garnish: 'Orange Wheel'
+    }
+    
+];
